fc7c6fb48fc8696de0367748410e0b4eec967eab
xeta <ivankotenko@gmail.com (mailto:%22ivankotenko@gmail.com)>
2011-03-07 14:07:56 +0300

    drw rectangle

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/7b/20a64123aa480010160184e1a54622f1
@@ -0,0 +1,12 @@
+package com.superdeveloperz.supergame;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameActivity extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
---
+++ bhbhjb/src/jhnhn/ggug/hiuh.java
@@ -0,0 +1,13 @@
+package jhnhn.ggug;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class hiuh extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
